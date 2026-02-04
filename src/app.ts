0dc9eb02b4e541f738e09c9e32e67b7d3993ef3d
lazier334 <lazier334@lazier334.com>
2026-02-04 16:10:51 +0800

    fix: = 修复重启接口无效

--- src/app.ts
+++ src/app.ts
@@ -11,11 +11,30 @@ import { plugins } from './libs/plugins.ts';
     await (await plugins('systemStart')).use({ fs, path, config, app });
     // 初始化 koa
     await initKoa(app);
-    // 创建 HTTPS 服务器
-    https.createServer(config.SSLOptions, app.callback()).listen(config['portHttps'],
-        () => console.log(`https 服务器已运行，访问地址:  \x1b[33m https://localhost:${config['portHttps']} \x1b[0m`));
-    // 创建 HTTP 服务器
-    app.listen(config['portHttp'], () => console.log(`http  服务器已运行，访问地址:    http://localhost:${config['portHttp']}`));
-    // 创建 WebSocket 服务器
-    websocketServer(config);
+    // 启动监听端口空闲后启动服务器
+    waitPortListen();
+
+    /** 
+     * 等待端口监听，每间隔 1 秒尝试一次  
+     * 默认会等待3秒时间进行总共3次轮询去绑定特定的端口
+     * @param {number} [num=3] 等待次数
+     * @param {https.Server} server 服务器对象
+     */
+    function waitPortListen(num: number = 3, server: any = null): void {
+        if (!server) {
+            server = https.createServer(config.SSLOptions, app.callback());
+            server.on('error', (error: any) => {
+                if (0 < num) return setTimeout(() => waitPortListen(num - 1), 1000);
+                else throw error;
+            });
+        }
+        // 创建 HTTPS 服务器
+        server.listen(config['portHttps'], () => {
+            console.log(`https 服务器已运行，访问地址:  \x1b[33m https://localhost:${config['portHttps']} \x1b[0m`);
+            // 创建 HTTP 服务器
+            app.listen(config['portHttp'], () => console.log(`http  服务器已运行，访问地址:    http://localhost:${config['portHttp']}`));
+            // 创建 WebSocket 服务器
+            websocketServer(config);
+        });
+    }
 })()
