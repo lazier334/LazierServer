2c92bda10ecd35edcf41ed6c1206ac01f6cc176c
lazier334 <lazier334@lazier334.com>
2026-02-10 16:28:19 +0800

    fix: = 修复类型错误提示

--- src/libs/initKoa.ts
+++ src/libs/initKoa.ts
@@ -12,18 +12,19 @@ export default initKoa;
  */
 async function initKoa(app: Koa): Promise<void> {
     // 添加路由
-    app.use(async (ctx: Koa.DefaultContext, next: Koa.Next) => await koaCompose((await plugins('koaPlugin')).data)(ctx, next))
-        .use(async (ctx: Koa.DefaultContext, next: Koa.Next) => {
-            // 动态路由
-            const routers = await readKoaRouters();
-            if (config.switch.dynamicRouter && routers.match(ctx.path, ctx.method).route) {
-                // 路由匹配成功，执行这里的内容
-                const routersMiddleware = routers.routes();
-                let re = await routersMiddleware(ctx, next);
-                if (!ctx.next) return re;
-                delete ctx.next;
-            }
-            // 路由匹配失败或者存在 ctx.next 时，走传统路由
-            return await next();
-        }).use(completeFile);
+    app.use(async (ctx: Koa.DefaultContext, next: Koa.Next) => {
+        return await koaCompose((await plugins('koaPlugin')).data)(ctx as any, next)
+    }).use(async (ctx: Koa.DefaultContext, next: Koa.Next) => {
+        // 动态路由
+        const routers = await readKoaRouters();
+        if (config.switch.dynamicRouter && routers.match(ctx.path, ctx.method).route) {
+            // 路由匹配成功，执行这里的内容
+            const routersMiddleware = routers.routes();
+            let re = await routersMiddleware(ctx as any, next);
+            if (!ctx.next) return re;
+            delete ctx.next;
+        }
+        // 路由匹配失败或者存在 ctx.next 时，走传统路由
+        return await next();
+    }).use(completeFile);
 }
