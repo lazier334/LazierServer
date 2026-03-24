3a5a91439c62bf67ba36544107b208bb9e56a381
lazier334 <lazier334@lazier334.com>
2026-03-24 10:57:25 +0800

    chore: = 将 StageClass 类合并到 plugins.ts 中

--- src/libs/plugins.ts
+++ src/libs/plugins.ts
@@ -1,4 +1,3 @@
-import Stage from './StageClass.ts';
 import { pathToFileURL } from 'url';
 import { fs, path, config } from "./config.ts";
 
@@ -19,6 +18,44 @@ declare global {
     }
 }
 
+/**
+ * 阶段类
+ */
+class Stage {
+    /** 阶段名称 */
+    stage: string;
+    /** 更新时间 */
+    updateTime: number;
+    /** 插件列表 */
+    data: any[] = [];
+
+    /**
+     * 创建阶段对象
+     * @param stage - 阶段名称
+     */
+    constructor(stage: string) {
+        this.stage = stage;
+        this.updateTime = Date.now();
+    }
+
+    /** 
+     * 使用函数，当返回 `{end:true, result:any}` 时停止后续执行并返回 `result` 数据
+     * @returns {any} 默认返回第一个参数
+     */
+    async use(...args: any[]): Promise<any> {
+        if (this.data.length < 1) {
+            console.warn(this.stage + ' 阶段的插件列表为空');
+        } else {
+            for (const handle of this.data) {
+                const re = await handle(...args);
+                if (re?.end) {
+                    return re.result;
+                }
+            }
+        }
+        return args[0];
+    }
+}
 /**
  * 这是插件化的核心内容，提供各个阶段的插件，阶段名称自取即可，编写插件使用前缀+任意+.js即可，例如 `systemStart-morePlugin.js`  
  * 阶段示例:
