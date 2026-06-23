fdd8f88bccdadcaa88967039ecae11dd6f17c99e
lazier334 <lazier334@lolo.hk>
2026-06-23 20:16:03 +0800

    chore: = 命令优化

--- src/program.ts
+++ src/program.ts
@@ -11,6 +11,24 @@ const runfile = path.join(__dirname, 'start.log');
 const program = new Command();
 /** 在主程序中运行的函数，此时已经初始化config完成 */
 var callback = (main: () => Promise<void>, config: typeof defConfig) => { };
+const getStartCallback = (options: any) => {
+    return ((main, config) => {
+        // 检查如果运行的路径不是当前程序的路径里，那么就将其添加到当前的配置中
+        if (!nowDir.replaceAll('/', '').replaceAll('\\', '').includes(lsDir.replaceAll('/', '').replaceAll('\\', ''))) {
+            console.info('将当前文件夹作为web与plugin目录', nowDir);
+            config.otherWebPath.push(nowDir);
+            config.pluginDirs.push(nowDir);
+        }
+        // 修改端口
+        options.port = Number(options.port)
+        if (0 < options.port) {
+            config.portHttp = options.port;
+            config.portHttps = options.port + 1;
+        }
+        // 启动
+        main();
+    }) as typeof callback
+}
 
 program.name('lazierserver')
     .description('LaizerServer 服务器')
@@ -20,32 +38,15 @@ program.command('start', { isDefault: true })
     .option('-p, --port <number>', '指定端口号(https默认使用该端口+1作为配置)', '0')
     .description('启动服务器')
     .action(async (options) => {
-        callback = (main, config) => {
-            // 检查如果运行的路径不是当前程序的路径里，那么就将其添加到当前的配置中
-            if (!nowDir.replaceAll('/', '').replaceAll('\\', '').includes(lsDir.replaceAll('/', '').replaceAll('\\', ''))) {
-                console.info('将当前文件夹作为web与plugin目录', nowDir);
-                config.otherWebPath.push(nowDir);
-                config.pluginDirs.push(nowDir);
-            }
-            // 修改端口
-            options.port = Number(options.port)
-            if (0 < options.port) {
-                config.portHttp = options.port;
-                config.portHttps = options.port + 1;
-            }
-            // 启动
-            main();
-        }
+        callback = getStartCallback(options)
     });
 
 program.command('restart').aliases(['reboot'])
     .option('-p, --port <number>', '指定端口号(https默认使用该端口+1作为配置)', '0')
     .description('重启服务器')
-    .action(async () => {
+    .action(async (options) => {
         fs.unlinkSync(runfile);
-        callback = (main, config) => {
-            main();
-        }
+        callback = getStartCallback(options)
     });
 
 program.command('clean').aliases(['clear'])
