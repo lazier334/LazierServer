2bbda5d4487033cd52792eb229c712badcabce77
lazier334 <lazier334@lolo.hk>
2026-07-27 20:09:53 +0800

    chore: = 清除 selectFileByDomainsDemo 的提前结束

--- ld/lazier334/selectFileByDomains-demo.js
+++ ld/lazier334/selectFileByDomains-demo.js
@@ -6,5 +6,5 @@ import { createSelectFileByDomains } from './types/index.js';
  * 多路径存在同一api时的选择算法插件的demo
  */
 export default createSelectFileByDomains(function selectFileByDomainsDemo(domains, domainsMap, ctx) {
-    return { end: true, result: domains[0] }
+    // return { end: true, result: domains[0] }
 })
