f373bc1c620cde554d318aeedbb30daf5ed06e90
lazier334 <lazier334@lolo.hk>
2026-07-29 16:27:05 +0800

    chore: = 优化命令执行函数

--- ld/lazier334/libs/sys-restart.js
+++ ld/lazier334/libs/sys-restart.js
@@ -8,24 +8,26 @@ export { runCmd, restartSystem }
  * @param {'../../bin/start.bat'} startCmd 启动命令
  */
 function restartSystem() {
-    runCmd(process.argv[0], process.argv.slice(1), () => process.exit(0))
+    runCmdAsync(process.argv[0], process.argv.slice(1)).then(() => process.exit(0))
 }
 
 /**
- * 
- * @param {string} command 命令
- * @param {[string]} args 参数
- * @param {()=>{}} spawnCallback 子进程启动后1秒触发这里
- * @param {(code)=>{}} closeCallback code==0则为正常运行结束，结束1秒后才运行这个回调
+ * 异步开启子线程运行命令
+ * @param command 命令
+ * @param args 参数
+ * @param no1sResolve 不进行1秒 resolve
+ * @param resolve 默认情况下运行1秒后会执行该回调，code==0则为正常运行结束，结束1秒后才运行这个回调
  */
-function runCmd(command, args, spawnCallback = () => { }, closeCallback = () => { }) {
-    console.log('运行命令:', command, args);
-    const child = spawn(command, args, {
-        stdio: 'inherit',   // 将输出重定向到当前控制台
-        shell: true,        // 使用 shell 执行命令
+function runCmdAsync(command, args, no1sResolve = false) {
+    return new Promise((resolve, reject) => {
+        console.log('运行命令:', command, args);
+        const child = spawn(command, args, {
+            stdio: 'inherit',   // 将输出重定向到当前控制台
+            shell: true,        // windows需要使用 shell 执行命令
+        });
+        // clsoe事件需要等待启动的程序运行结束，所以一般不会走这里面的代码
+        child.on('close', (code) => setTimeout(() => resolve(code), 1000));
+        // 确保子进程启动后再退出当前进程，不能立刻退出
+        if (!no1sResolve) child.on('spawn', () => setTimeout(resolve, 1000));
     });
-    // clsoe事件需要等待启动的程序运行结束，所以一般不会走这里面的代码
-    child.on('close', (code) => setTimeout(() => closeCallback(code), 1000));
-    // 确保子进程启动后再退出当前进程，不能立刻退出
-    child.on('spawn', () => setTimeout(spawnCallback, 1000));
 }
