5df850b181ac82ec188eeb3a897c6e4fb33bc048
lazier334 <lazier334@lolo.hk>
2026-09-18 10:29:00 +0800

    v1.3.16-26091800 + 增加实例测试 - 取消启动时检测版本 = 优化系统

--- src/libs/version.ts
+++ src/libs/version.ts
@@ -1,4 +1,8 @@
 export default Object.fromEntries(Object.entries({
+    "1.3.16-26091800": `+ 增加实例测试
+- 取消启动时检测版本
+= 优化系统
+`,
     "1.3.15-26091002": `= 优化系统
 `,
     "1.3.14-26090502": `= 修复扫描har时因为api存在正则符号导致触发path-to-regexp模块并报错的问题
